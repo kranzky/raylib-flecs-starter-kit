2c06cb641ee430828edcf8a2d4ecf83ca972a58d
kranzky <jasonhutchens@gmail.com>
2021-09-02 17:16:01 +0800

    build and debug working for vscode windows

--- src/scenes/level.c
+++ src/scenes/level.c
@@ -9,7 +9,7 @@
 void init_level(ecs_world_t *world, const Scene *scene, ecs_entity_t parent)
 {
   Vector2 position = {RASTER_WIDTH * 0.5, RASTER_HEIGHT * 0.5};
-  entity_manager_spawn_label(world, parent, FONT_CLOVER, "Game Level", ALIGN_CENTRE, VALIGN_MIDDLE, 50, position, GREEN);
+  entity_manager_spawn_label(world, parent, FONT_CLOVER, "Yay Play", ALIGN_CENTRE, VALIGN_MIDDLE, 50, position, GREEN);
 }
 
 //------------------------------------------------------------------------------
